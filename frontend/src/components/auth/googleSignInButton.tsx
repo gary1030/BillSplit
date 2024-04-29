ea2ed2f2bc81579cdd0de789ee2da30b56e67c9f
naush01 <r12922a05@ntu.edu.tw>
2024-04-29 11:51:44 +0800

    landing page: add hover in google sign in button

--- frontend/src/components/auth/googleSignInButton.tsx
+++ frontend/src/components/auth/googleSignInButton.tsx
@@ -9,8 +9,34 @@ interface GoogleSignInButtonProps {
 const GoogleSignInButton = () => {
   const { login } = useAuth();
   return (
-    <Button variant="unstyled" onClick={() => login()} padding="0">
-      <Image src="/images/googleSignInButton.svg" alt="Continue with Google" />
+    <Button
+      variant="unstyled"
+      onClick={() => login()}
+      padding="0"
+      position="relative"
+      borderRadius="10px"
+      overflow="hidden"
+      _hover={{
+        _after: {
+          content: '""',
+          position: "absolute",
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          bg: "blackAlpha.200",
+          borderRadius: "inherit",
+        },
+      }}
+    >
+      <Image
+        src="/images/googleSignInButton.svg"
+        alt="Continue with Google"
+        fit="cover"
+        align="center"
+        w="full"
+        h="full"
+      />
     </Button>
   );
 };
