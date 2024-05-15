b2940baabf94f5019867d9afc7119460d63faa54
gary1030 <gary6658@ntu.im>
2024-05-15 14:44:58 +0800

    feat: extend token expiration

--- frontend/src/actions/login.ts
+++ frontend/src/actions/login.ts
@@ -4,6 +4,8 @@ import httpAgent from "@/agent/httpAgent";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
+const expiration = 60 * 60 * 24 * 7 * 2; // 2 week
+
 export default async function login(code: string) {
   try {
     const data = await httpAgent("/auth/login", {
@@ -13,10 +15,10 @@ export default async function login(code: string) {
       },
       body: JSON.stringify({ code: code }),
     });
-    cookies().set("token", data.token);
-    cookies().set("username", data.username);
-    cookies().set("email", data.email);
-    cookies().set("userId", data.id);
+    cookies().set("token", data.token, { maxAge: expiration });
+    cookies().set("username", data.username, { maxAge: expiration });
+    cookies().set("email", data.email, { maxAge: expiration });
+    cookies().set("userId", data.id, { maxAge: expiration });
   } catch (error) {
     console.error("Failed to login:", error);
   }
